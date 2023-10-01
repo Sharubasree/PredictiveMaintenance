7a7f9f3541c091d3d58580d06edf5b9168fdd423
Subasree-Vivekanandan <subasreev111@gmail.com>
2023-10-01 23:41:18 +0530

    models imported fixed bugs

--- app.py
+++ app.py
@@ -1,1 +1,58 @@
+# -*- coding: utf-8 -*-
 
+import pandas as pd
+from flask import Flask, request,render_template
+import pickle as pkl
+
+app = Flask(__name__)
+log_reg_model = pkl.load(open('logisticreg.pkl','rb'))
+dec_tree_model = pkl.load(open('decisiontree.pkl','rb'))
+rndm_frst_model = pkl.load(open('randomforest.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/logreg')
+def logreg_page():
+    return render_template('lr_upload.html')
+
+@app.route('/lrpredict',methods=['POST'])
+def predict_logreg(): 
+    return render_template('check_p.html')
+    
+@app.route('/dectr')
+def decisiontree_page():
+    return render_template('dt_upload.html')
+
+@app.route('/dtpredict',methods=['POST'])
+def predict_decisiontree(): 
+    return render_template('check_p.html')
+
+@app.route('/rdmfrst')
+def randomfrst_page():
+    return render_template('rdmfrst_upload.html')
+
+@app.route('/rfpredict',methods=['POST'])
+def predict_randomfrst(): 
+    return render_template('check_p.html')
+
+#@app.route('/upload')
+#def upload_route_summary():
+#    return render_template('upload.html')
+
+#@app.route('/file',methods=['POST'])
+#def file_upload():
+#       f = request.files.get('fileupload')
+#       df = pd.read_csv(f, encoding='latin-1')
+#       output = df.to_numpy()
+#       output_arr = np.array(output)
+       
+#       return render_template('display.html', dict_output=output_arr)
+    
+if __name__ == "__main__":
+    app.run(debug=True)    
+        
+    
+    
+    
